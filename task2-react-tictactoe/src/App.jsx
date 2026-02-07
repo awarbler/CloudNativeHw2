579576accac26d525a14558475d49a0106b916c4
awarbler <awoodfor@byui.edu>
2026-02-07 09:04:07 -0600

    Task 2: lift squares state to Board (Fixes #10)

--- task2-react-tictactoe/src/App.jsx
+++ task2-react-tictactoe/src/App.jsx
@@ -4,16 +4,14 @@ import React, {useState } from "react"; // imports the useState hook from the re
 import "./App.css";// loads the CSS file for styling the app
 
 // define a reusable square component that represents each cell in the tic-tac-toe grid
-function Square() { // defines a function component named square
+function Square({value, onSquareClick}) { // defines a function component named square
   // create a state for square
-  const [value , setValue] =useState(null); // initializes a state variable named value with an initial value of null, and a function named setValue that can be used to update the value of the state variable
+  //const [value , setValue] =useState(null); // initializes a state variable named value with an initial value of null, and a function named setValue that can be used to update the value of the state variable
   // define click handler inside of square component
-  function handleClick() { // defines a function named handleClick that will be called when the square is clicked
-    setValue("X"); // updates the value of the state variable to "X" when the square is clicked
-    //console.log(`Square ${value} clicked!`); // logs a message to the console indicating which square was clicked
-  }
   return ( // returns jsx that renders a button element with the class name "square"
-    <button className="square" onClick={handleClick}
+    <button 
+      className="square" 
+      onClick={onSquareClick}
     >
       {value} {/* show a value in each square 1-9*/}
       </button>
@@ -21,25 +19,32 @@ function Square() { // defines a function component named square
 }
 
 // define the board component that renders a 3x3 grid of squares
-function Board() { // define the board component that renders a 3x3 grid of squares
+function Board() { // define the board component that renders a 3x3 grid of squares function handleClick() 
+  const [squares, setSquares] = useState(Array(9).fill(null)); // initializes a state variable named squares with an initial value of an array of 9 null values, and a function named setSquares that can be used to update the value of the state variable 
+
+  function handleClick(i) { // defines a function named handleClick that will be called when the square is clicked
+    const nextSquares = squares.slice(); // creates a copy of the squares array using the slice method and assigns it to a new variable named nextSquares
+    nextSquares[i] = "X"; // updates the value of the square at index i in the nextSquares array to "X"
+    setSquares(nextSquares); // updates the value of the squares state variable to the new array created in the previous step
+  }
+
   return (
     <div>
       <div className="board-row"> {/* first row of the board */}
-        <Square /> {/* square 0 */}
-        <Square /> {/* square 1 */}
-        <Square /> {/* square 2 */} 
-
+        <Square value={squares[0]} onSquareClick={() => handleClick(0)} /> {/* square 0 */}
+        <Square value={squares[1]} onSquareClick={() => handleClick(1)} /> {/* square 1 */}
+        <Square value={squares[2]} onSquareClick={() => handleClick(2)} /> {/* square 2 */}
       </div>
       <div className="board-row"> {/* second row of the board */}
-        <Square /> {/* square 3 */}
-        <Square /> {/* square 4 */}
-        <Square /> {/* square 5 */}
+        <Square value={squares[3]} onSquareClick={() => handleClick(3)} /> {/* square 3 */}
+        <Square value={squares[4]} onSquareClick={() => handleClick(4)} /> {/* square 4 */}
+        <Square value={squares[5]} onSquareClick={() => handleClick(5)} /> {/* square 5 */}  
 
       </div>
       <div className="board-row"> {/* third row of the board */}
-        <Square /> {/* square 6 */}
-        <Square /> {/* square 7 */}
-        <Square /> {/* square 8 */}
+        <Square value={squares[6]} onSquareClick={() => handleClick(6)} /> {/* square 6 */}
+        <Square value={squares[7]} onSquareClick={() => handleClick(7)} /> {/* square 7 */}
+        <Square value={squares[8]} onSquareClick={() => handleClick(8)} /> {/* square 8 */}
       </div>
 
     </div>
