ada9672f35f64a997f1dae8e871432f435f9a9ce
awarbler <awoodfor@byui.edu>
2026-02-07 12:22:42 -0600

    Task 3: install Material UI (Fixes #22)

---
+++ task3-projects-page/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Projects from "./components/Projects";
+
+function App() {
+  return (
+    <div className="App">
+        <Projects /> {/* renders the Projects component which displays the list of projects */}
+    </div>
+  );
+}
+
+export default App;
---
+++ task3-projects-page/src/components/Projects.jsx
@@ -0,0 +1,10 @@
+function Projects() {
+    return ( // return the UI for the projects page
+        <div>
+            <h1>Projects</h1> {/ * page title * /}
+            <p> Task 3 Projects page will go here.</p> {/ * placeholder text for now * / }
+        </div>
+        );
+}
+
+export default Projects;
